e89f5872fbfed8ceb98c1dc17665a17071c9f0ca
will <willamssouza917@gmail.com>
2023-02-07 23:32:10 -0300

    Criando um servidor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello!!!");
+import  express, {Request, Response, NextFunction}  from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send("Hello world!!")
+});
+
+app.listen(port, () => {
+    console.log(`Runing in port ${port}`)
+});
